c16a6c9467295acc609f53284cc77994a1775aa7
josemanuelnl <52866412+josemanuelnl@users.noreply.github.com>
2025-08-27 18:52:10 -0400

    Test Execution OMS-127
    Preconditions for test execution OMS-127

---
+++ Preconditions/OMS-127.sql
@@ -0,0 +1,14 @@
+INSERT IGNORE INTO Products(ID, IsProductActive, ProductName, ProductDescription, ProductPrice) VALUES
+(1, 1, 'ProductName1', 'ProductDescription1', 3),
+(2, 1, 'ProductName2', 'ProductDescription2', 1),
+(3, 1, 'ProductName3', 'ProductDescription3', 5),
+(4, 1, 'ProductName4', 'ProductDescription4', 2),
+(5, 1, 'ProductName5', 'ProductDescription5', 4),
+(6, 1, 'ProductName6', 'ProductDescription6', 1),
+(7, 1, 'ProductName7', 'ProductDescription7', 5),
+(8, 1, 'ProductName8', 'ProductDescription8', 2),
+(9, 1, 'ProductName9', 'ProductDescription9', 3),
+(10, 1, 'ProductName10', 'ProductDescription10', 4),
+(11,1,'Banana','Fruit',2),
+(12,1,'BananaSplitSyrup1234','Delicious ice cream',5),
+(13,1,'Z','x',5);
